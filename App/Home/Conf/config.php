4b3a57f3477e2e999d93199d15e82e4df07928ba
atshike <atshike@foxmail.com>
2016-04-28 19:07:34 +0800

    Update config.php

--- App/Home/Conf/config.php
+++ App/Home/Conf/config.php
@@ -4,9 +4,9 @@ return array(
 	
 	'DB_TYPE'=>'mysql',
 	'DB_HOST'=>'localhost',
-	'DB_NAME'=>'px_lyj0',
-	'DB_USER'=>'px_lyj0',
-	'DB_PWD'=>'px_lyj5566',
+	'DB_NAME'=>'dbname',
+	'DB_USER'=>'dbuser',
+	'DB_PWD'=>'dbpassword',
 	'DB_CHARSET'=>'utf8',
 
 //'TMPL_EXCEPTION_FILE'=>'./Public/error.html',
