f69a2ca57a95698c2dc8d6af0a74859a0a16d6f7
atshike <atshike@foxmail.com>
2016-09-24 12:23:18 +0800

    Update WeixinController.php

--- App/Home/Controller/WeixinController.php
+++ App/Home/Controller/WeixinController.php
@@ -1,208 +1,201 @@
 <?php
+
 /**
  * 微信Api
+ * Class WeixinController
  */
-
-define("TOKEN", "atshike");
-$wechatObj = new wechatCallbackapiTest();
-$wechatObj->inserMsg();
-
-class wechatCallbackapiTest
+class WeixinController
 {
-    public function valid()
+    private $WxObj;
+    private $appid = "wx9d57e78b5bda6755";
+    private $appsecret = "766f02a79683557a23cbaed0fc9024d5";
+
+    public function index()
     {
-        $echoStr = $_GET["echostr"];
-        if ($this->checkSignature()) {
-            echo $echoStr;
-            exit;
+        $token = 'atshike';
+        //验证
+        $this->verify($token);
+        //获取消息
+        $WxObj = $this->getWxObj();
+        $this->WxObj = $WxObj;
+        //关注
+        if ($this->isSubscribe()) {
+            $this->responseMsg('欢迎您关注！\n回复1：显示最新新闻\n回复2：显示最新活动\n回复3：显示地图\n我的主页：www.obo1.com');
+        }
+        //回复
+        if ($this->isText()) {
+            switch (trim($this->WxObj->Content)) {
+                case '1':
+                    $this->responseMsg('1:新闻');
+                    break;
+                case '2':
+                    $this->responseMsg($this->news());
+                    break;
+                default:
+                    $this->responseMsg('请输入点什么吧！');
+                    break;
+            }
         }
     }
 
-    public function responseMsg()
+    /**
+     * 获取accessToken
+     */
+    private function getaccessToken()
+    {
+        $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=" . $this->appid . "&secret=" . $this->appsecret;
+        $output = $this->https_request($url);
+        $jsoninfo = json_decode($output, true);
+        $access_token = $jsoninfo["access_token"];
+        $jsonmenu = '{
+    "button":[
+    {
+        "name":"程序开发",
+       "sub_button":[
+        {
+           "type":"view",
+           "name":"PHP开发",
+           "url":"http://www.obo1.com/"
+        },
+        {
+           "type":"view",
+           "name":"laravel开发",
+           "url":"http://www.obo1.com/"
+        }]
+    },
+    {
+       "name":"Web前端",
+       "sub_button":[
+        {
+           "type":"view",
+           "name":"CSS",
+           "url":"http://www.obo1.com/"
+        },
+        {
+           "type":"view",
+           "name":"Jquery",
+           "url":"http://www.obo1.com/"
+        }]
+    },{
+        "type": "scancode_push", 
+        "name": "扫一扫", 
+        "key": "rselfmenu_0_1", 
+        "sub_button": []
+      }
+    ]
+ }';
+        $url = "https://api.weixin.qq.com/cgi-bin/menu/create?access_token=" . $access_token;
+        $result = $this->https_request($url, $jsonmenu);
+        return $result;
+    }
+
+
+    /**
+     * 关注事件
+     */
+    private function isSubscribe()
+    {
+        if ($this->WxObj->MsgType == 'event') {
+            if ($this->WxObj->Event == 'subscribe') {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 返回文本数据
+     */
+    private function responseMsg($msg)
+    {
+        $WxObj = $this->WxObj;
+        $toUser = $WxObj->FromUserName;
+        $fromUser = $WxObj->ToUserName;
+        $time = time();
+        $str = <<<str
+        <xml>
+         <ToUserName><![CDATA[{$toUser}]]></ToUserName>
+         <FromUserName><![CDATA[{$fromUser}]]></FromUserName>
+         <CreateTime>{$time}</CreateTime>
+         <MsgType><![CDATA[text]]></MsgType>
+         <Content><![CDATA[{$msg}]></Content>
+         </xml>
+str;
+        echo $str;
+        die;
+
+
+    }
+
+    /**
+     * 获取消息类型
+     */
+    private function isText()
+    {
+        if ($this->WxObj->MsgType == 'text') {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 获取消息
+     */
+    private function getWxObj()
     {
         $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
-        if (!empty($postStr)) {
-            libxml_disable_entity_loader(true);
-            $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
-            $fromUsername = $postObj->FromUserName;
-            $toUsername = $postObj->ToUserName;
-            $keyword = trim($postObj->Content);
-            $time = time();
-            $textTpl = "<xml>
-                        <ToUserName><![CDATA[%s]]></ToUserName>
-                        <FromUserName><![CDATA[%s]]></FromUserName>
-                        <CreateTime>%s</CreateTime>
-                        <MsgType><![CDATA[%s]]></MsgType>
-                        <Content><![CDATA[%s]]></Content>
-                        <FuncFlag>0</FuncFlag>
-                        </xml>";
-            if (!empty($keyword)) {
-                $msgType = "text";
-                $contentStr = "欢迎访问我的博客！www.obo1.com";
-                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-                echo $resultStr;
-            } else {
-                echo "请输入内容，查看相关信息";
-            }
-
-        } else {
-            echo "发送内容为空";
-            exit;
-        }
+        file_put_contents('xml.php', $postStr);//
+        libxml_disable_entity_loader(true);
+        $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+        return $postObj;
     }
 
     /**
-     * 自动回复内容
-     * 接收内容，写入数据库
+     * 验证
+     * @param $token
+     * @return bool
+     * http://www.abc.com/weixin/index.php?action=index
      */
-    public function inserMsg()
+    private function verify($token)
     {
-        $data = $GLOBALS["HTTP_RAW_POST_DATA"];
-        $postObj = simplexml_load_string($data);
-        if (!empty($postObj)) {
-            $toUsername = $postObj->ToUserName;
-            $fromUsername = $postObj->FromUserName;
-            $CreateTime = $postObj->CreateTime;
-            $type = $postObj->MsgType;
-
-            /**
-             * 接收数据
-             * $textTpl = "<xml>
-             * <ToUserName><![CDATA[%s]]></ToUserName>
-             * <FromUserName><![CDATA[%s]]></FromUserName>
-             * <CreateTime>%s</CreateTime>
-             * <MsgType><![CDATA[%s]]></MsgType>
-             * <Content><![CDATA[%s]]></Content>
-             * <FuncFlag>0</FuncFlag>
-             * </xml>";
-             */
-
-            if ($type == "text") {
-                $Content = $postObj->Content;
-                $tpl = "<xml>
-                        <ToUserName><![CDATA[%s]]></ToUserName>
-                        <FromUserName><![CDATA[%s]]></FromUserName>
-                        <CreateTime>%s</CreateTime>
-                        <MsgType><![CDATA[%s]]></MsgType>
-                        <Content><![CDATA[%s]]></Content>
-                        <FuncFlag>0</FuncFlag>
-                        </xml>";
-                $toUser = $fromUsername;
-                $fromUser = $toUsername;
-                $time = $CreateTime;
-                $MsgType = "text";
-                if ($Content == "1" || $Content == "新闻") {
-                    $rep = $this->news();
-                } elseif ($Content == "2" || $Content == "活动") {
-                    $rep = "最新活动,此处省略3千字...";
-                } elseif ($Content == "3" || $Content == "地图") {
-                    $rep = "你在银河系，火星上...";
-                } else {
-                    $rep = "您可以发送：1/新闻：查看新闻；2/活动：查看活动；3/地图：查看当前地里位置。";
-                }
-                mysqli_query('SET NAMES utf8');
-                $con = mysqli_connect("localhost", "root", "test.", "weixin");
-                $sql = "INSERT INTO `weixin` (`id`, `touser`, `fromuser`, `createtime`, `content`) VALUES ('','$toUser','$fromUser','$time','$Content')";
-                if (mysqli_query($con, $sql)) {
-                    $str = sprintf($tpl, $toUser, $fromUser, $time, $MsgType, $rep);
-                    if (!empty($str)) {
-                        echo $str;
-                    }
-                }
-                $id = $postObj->MsgId;
-                mysqli_close($con);
-            }
-            /**
-             * 关注事件
-             */
-            if ($type == "event") {
-                $event = $postObj->Event;
-                if ($event == "subscribe") {
-                    $tpl = "<xml>
-                            <ToUserName><![CDATA[%s]]></ToUserName>
-                            <FromUserName><![CDATA[%s]]></FromUserName>
-                            <CreateTime>%s</CreateTime>
-                            <MsgType><![CDATA[%s]]></MsgType>
-                            <Content><![CDATA[%s]]></Content>
-                            <FuncFlag>0</FuncFlag>
-                            </xml>";
-                    $toUser = $fromUsername;
-                    $fromUser = $toUsername;
-                    $time = time();
-                    $MsgType = "text";
-                    $rep = "欢迎您关注！\n回复1：显示最新新闻\n回复2：显示最新活动\n回复3：显示地图\n我的主页：www.obo1.com";
-                    $str = sprintf($tpl, $toUser, $fromUser, $time, $MsgType, $rep);
-                    if (!empty($str)) {
-                        echo $str;
-                    }
-                }
-            }
-            /**
-             * 地图
-             */
-            if ($type == "location") {
-                $Location_X = $postObj->Location_X;
-                $Location_Y = $postObj->Location_Y;
-                $Scale = $postObj->Scale;  //地图缩放
-                $Label = $postObj->Label;  //地理位置信息
-
-                $tpl = "<xml>
-						<ToUserName><![CDATA[%s]]></ToUserName>
-						<FromUserName><![CDATA[%s]]></FromUserName>
-						<CreateTime>%s</CreateTime>
-						<MsgType><![CDATA[%s]]></MsgType>
-						<Content><![CDATA[%s]]></Content>
-						<FuncFlag>0</FuncFlag>
-						</xml>";
-                $toUser = $fromUsername;
-                $fromUser = $toUsername;
-                $time = time();
-                $rep = $Location_X . ":" . $Location_Y;
-                $MsgType = "text";
-                $str = sprintf($tpl, $toUser, $fromUser, $time, $MsgType, $rep);
-                $urlstr = "http://api.map.baidu.com/place/v2/search?query=酒店&page_size=10&page_num=0&scope=1&location=34.249805,117.244446&radius=2000&output=json&ak=F7VUBhAI1LweHCw1WsLziEcxnLKGPpgG";
-                $jsonstr = file_get_contents($urlstr);
-                $json = json_decode($jsonstr, true);
-                echo $json['message'];
-            }
-        }
-    }
-
-    /**
-     * @param $signature
-     * @param $timestamp
-     * @param $nonce
-     * 微信Api
-     * 验证 $token
-     */
-    private function checkSignature()
-    {
-        if (!defined("TOKEN")) {
-            throw new Exception('TOKEN is not defined!');
-        }
+        $echoStr = $_GET["echostr"];
         $signature = $_GET["signature"];
         $timestamp = $_GET["timestamp"];
         $nonce = $_GET["nonce"];
-        $token = TOKEN;
         $tmpArr = array($token, $timestamp, $nonce);
         sort($tmpArr, SORT_STRING);
         $tmpStr = implode($tmpArr);
         $tmpStr = sha1($tmpStr);
-        if ($tmpStr == $signature) {
-            return true;
+        if ($tmpStr == $signature && isset($_GET['echoStr'])) {
+            echo $echoStr;
         } else {
             return false;
         }
     }
-    
-    
-    public function news()
+
+
+    /**
+     * @return mysqli
+     */
+    private function conn()
+    {
+        mysqli_query('SET NAMES utf8');
+        $con = mysqli_connect("localhost", "root", ".", "test");
+        return $con;
+    }
+
+
+    /**
+     * 最新信息
+     * @return string
+     */
+    private function news()
     {
         $con = $this->conn();
         $sql = "select title from archives ORDER BY id desc limit 5";
         $result = $con->query($sql);
         if ($result->num_rows > 0) {
-            $rows =array();
+            $rows = array();
             while ($row = $result->fetch_assoc()) {
                 $rows[] = $row['title'];
             }
@@ -210,11 +203,21 @@ class wechatCallbackapiTest
         return "$rows[0] \n $rows[1] \n $rows[2] \n $rows[3] \n $rows[4]";
     }
 
-    private function conn(){
-        mysqli_query('SET NAMES utf8');
-        $con = mysqli_connect("localhost", "root", ".", "data");
-        return $con;
+    private function https_request($url, $data = null)
+    {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, FALSE);
+        if (!empty($data)) {
+            curl_setopt($curl, CURLOPT_POST, 1);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+        }
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        $output = curl_exec($curl);
+        curl_close($curl);
+        return $output;
     }
-}
 
-?>
+
+}
