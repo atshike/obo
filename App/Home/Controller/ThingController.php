26ac6dd2a0876cb200a91043f94f90753e404756
atshike <atshike@foxmail.com>
2016-10-08 15:10:08 +0800

    Update ThingController.php

--- App/Home/Controller/ThingController.php
+++ App/Home/Controller/ThingController.php
@@ -1,5 +1,11 @@
+/**
+*遍历一月的天数
+*/
+public function index()
+    {       
         $BeginDate=date('Y-m-01', strtotime(date("Y-m-d")));
         $daynum =  date('d', strtotime("$BeginDate +1 month -1 day"));
         for($i = 1; $i<=$daynum;$i++){
             echo date("Y-m").'-'.$i.'<br />';
         }
+}
